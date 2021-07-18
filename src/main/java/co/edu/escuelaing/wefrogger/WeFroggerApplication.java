733f63fe2c10264d76cfa215286e2f5550bdecfa
Desarik <daniel.mejia@mail.escuelaing.edu.co>
2021-07-17 19:27:49 -0500

    Pasando proyecto front con node.js a springboot

---
+++ src/main/java/co/edu/escuelaing/wefrogger/WeFroggerApplication.java
@@ -0,0 +1,13 @@
+package co.edu.escuelaing.wefrogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeFroggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeFroggerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/escuelaing/wefrogger/WeFroggerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.escuelaing.wefrogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeFroggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
